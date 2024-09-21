0cc0fbf3ca4f21639d42c09af84c27c2b908a43e
Kristian Ziu <k.ziu@binnova.it>
2024-09-21 02:50:27 +0200

    Added Seq logging with Serilog

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,14 @@
 using System.Reflection;
 using Carter;
+using Serilog;
 using VerticalSliceAPI.Extensions;
 using VerticalSliceAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(
+    (context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration)
+);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,12 +20,19 @@ builder.Services.AddCarter();
 
 var app = builder.Build();
 
+app.UseSerilogRequestLogging();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
     //app.ApplyMigrations();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
